6a364b8367d8d39c96d29b1ab47d0191e38176b8
Natesworks <gh@natesworks.com>
2024-09-14 17:19:13 +0200

    Change header on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="max-w-2xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4 text-gray-100">Welcome to My Portfolio</h1>
+      <h1 className="text-3xl font-bold mb-4 text-gray-100">Welcome!</h1>
       <p className="mb-4 text-gray-300">
         Hi! I&apos;m Natesworks. I am primarily a C, C++ and Python developer, but also know HTML, CSS and JavaScript.
       </p>
