b786f4d27e93da2ed88979e57d537d110f3a561f
Natesworks <gh@natesworks.com>
2024-09-14 17:44:36 +0200

    Align projects (formerly my projects) to left

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -21,7 +21,7 @@ const projects: Project[] = [
 export default function Projects() {
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-4xl font-bold mb-8 text-center text-gray-100">My Projects</h1>
+      <h1 className="text-4xl font-bold mb-8 text-gray-100">Projects</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
           <Link href={project.url} key={project.id} className="block">
